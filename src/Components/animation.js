bfb249354d5a1490dbbadefbb5b305d4b106ef71
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-08-20 04:55:29 -0700

    few additional chagges

--- src/Components/animation.js
+++ src/Components/animation.js
@@ -5,7 +5,7 @@ export const homeAnimation = {
 
 export const homeInfoAnimation = {
     hidden: {x: 100, opacity: 0},
-    show: {x: 0, opacity: 1}
+    show: {x: 0, opacity: 1},
 }
 
 export const navAnimation = {
